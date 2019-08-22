704a0cddcc75f7b82f207563cca3da45c105adca
ouzuyu <oupenguin@gmail.com>
2019-08-22 15:32:59 +0800

    perf: 动态路由匹配

--- template/route.js
+++ template/route.js
@@ -1,53 +1,56 @@
 import breadcrumbs from '@/components/breadcrumb/breadcrumb.config.json'
 
+function path2Arr(path) {
+  return path.split('/').filter(p => p)
+}
+
 export default ({ app, store }) => {
   app.router.beforeEach((to, from, next) => {
-    const paths = to.path
-      .split('/')
-      .filter(p => p)
-      .map(p => '/' + p)
+    const toPathArr = path2Arr(to.path)
+    const toPathArrLength = toPathArr.length
+    let matchPath = ''
 
-    const breadcrumbData = []
-
-    for (let i = 0; i < paths.length; i++) {
-      const curPath = ''
-      if (i) {
-        for (let j = 0; j <= i; j++) {
-          curPath += paths[j]
-        }
-      } else {
-        curPath = paths[i]
-      }
-
-      const breadcrumbObj = breadcrumbs.find(bread => bread.path === curPath)
-
-      // 若找到对应配置直接 push 进面包屑数组
-      if (breadcrumbObj) {
-        breadcrumbData.push(breadcrumbObj)
-      } else {
-        const paramKeys = Object.keys(to.params)
-        if (paramKeys.length) { // 动态路由
-          for (let k = 0; k < paramKeys.length; k++) {
-            const newPathArr = curPath.split('/').filter(p => p)
-            newPathArr[newPathArr.length - 1] = '_' + paramKeys[k]
-            const newPath =newPathArr.join('/')
-            const breadcrumbItem = breadcrumbs.find(bread => bread.path === '/' + newPath)
-            if (breadcrumbItem) {
-              breadcrumbData.push(breadcrumbItem)
-              paths[i] = '/' + newPathArr[newPathArr.length - 1]
-              break
-            }
-          }
-        } else { // 没有配置（没找到也不是动态路由）
-          breadcrumbData.push({
-            name: curPath.split('/').pop(),
-            path: curPath,
-            clickable: false,
-            isShow: true
-          })
-        }
+    // 从 matched 中找出当前路径的路由配置
+    for (let match of to.matched) {
+      const matchPathArr = path2Arr(match.path)
+      if (matchPathArr.length === toPathArrLength) {
+        matchPath = match.path
+        break
       }
     }
+
+    const breadcrumbData = path2Arr(matchPath)
+      .map(path => {
+        path = path.replace(/^:([^:?]+)(\?)?$/, (match, $1)=> {
+          return `_${$1}`
+        })
+        return '/' + path
+      })
+      .map((path, index, paths) => {
+
+        // 第 0 个不需拼接
+        if (index) {
+          let result = ''
+          for (let i = 0; i <= index; i++) {
+            result += paths[i]
+          }
+          return result
+        }
+        return path
+      })
+      .map(path => {
+        const item = breadcrumbs.find(bread => bread.path === path)
+        if (item) {
+          return item
+        }
+        return {
+          name: path.split('/').pop(),
+          path,
+          clickable: false,
+          isShow: true
+        }
+      })
+
     store.commit('breadcrumb/setBreadcrumb', breadcrumbData)
     next()
   })
