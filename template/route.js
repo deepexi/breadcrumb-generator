ff94f2a850d81bb91790e49da8677786747b83fd
joel <oupenguin@gmail.com>
2019-08-23 00:27:17 +0800

    refactor: route plugin抽离matchBreadcrumbData方法

--- template/route.js
+++ template/route.js
@@ -4,6 +4,40 @@ function path2Arr(path) {
   return path.split('/').filter(p => p)
 }
 
+function matchBreadcrumbData(matchPath) {
+  return path2Arr(matchPath)
+    .map(path => {
+      path = path.replace(/^:([^:?]+)(\?)?$/, (match, $1) => {
+        return `_${$1}`
+      })
+      return '/' + path
+    })
+    .map((path, index, paths) => {
+
+      // 第 0 个不需拼接
+      if (index) {
+        let result = ''
+        for (let i = 0; i <= index; i++) {
+          result += paths[i]
+        }
+        return result
+      }
+      return path
+    })
+    .map(path => {
+      const item = breadcrumbs.find(bread => bread.path === path)
+      if (item) {
+        return item
+      }
+      return {
+        name: path.split('/').pop(),
+        path,
+        clickable: false,
+        isShow: true
+      }
+    })
+}
+
 export default ({ app, store }) => {
   app.router.beforeEach((to, from, next) => {
     const toPathArr = path2Arr(to.path)
@@ -19,37 +53,7 @@ export default ({ app, store }) => {
       }
     }
 
-    const breadcrumbData = path2Arr(matchPath)
-      .map(path => {
-        path = path.replace(/^:([^:?]+)(\?)?$/, (match, $1)=> {
-          return `_${$1}`
-        })
-        return '/' + path
-      })
-      .map((path, index, paths) => {
-
-        // 第 0 个不需拼接
-        if (index) {
-          let result = ''
-          for (let i = 0; i <= index; i++) {
-            result += paths[i]
-          }
-          return result
-        }
-        return path
-      })
-      .map(path => {
-        const item = breadcrumbs.find(bread => bread.path === path)
-        if (item) {
-          return item
-        }
-        return {
-          name: path.split('/').pop(),
-          path,
-          clickable: false,
-          isShow: true
-        }
-      })
+    const breadcrumbData = matchBreadcrumbData(matchPath)
 
     store.commit('breadcrumb/setBreadcrumb', breadcrumbData)
     next()
