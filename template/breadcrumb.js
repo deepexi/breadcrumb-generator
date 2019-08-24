9d25a963c516d5680278d8ef425f639ceb12044a
ouzuyu <oupenguin@gmail.com>
2019-08-24 22:12:00 +0800

    feat: 新增动态修改某个面包屑名称

--- template/breadcrumb.js
+++ template/breadcrumb.js
@@ -5,5 +5,11 @@ export const state = () => ({
 export const mutations = {
   setBreadcrumb (state, breadcrumbData) {
     state.breadcrumbData = breadcrumbData
+  },
+
+  setBreadcrumbName(state, {oldName, newName}) {
+    let curBreadcrumb = state.breadcrumbData.find(breadcrumb => breadcrumb.name === oldName)
+
+    curBreadcrumb && (curBreadcrumb.name = newName)
   }
 }
